3202acb66298d6edb674d626476cae6dc7255de8
Yug Shende <yugshende@gwmail.gwu.edu>
2025-05-17 18:18:28 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,6 @@ use clap::{Parser, Subcommand};
 use dao::create_dev_log;
 use models::{DevLogArgs, NewDevLog};
 
-#[derive(Parser)]
-#[command(version, author, about = "Manage your dev logs, learning notes, and code snippets")]
-struct CliNotes {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Add a new development log entry
-    DevLog(DevLogArgs),
-use crate::dao::create_dev_log;
-use crate::models::DevLog;
-use clap::{Parser, Subcommand};
-
 #[derive(Parser, Debug)]
 #[command(
     version = "0.1",
